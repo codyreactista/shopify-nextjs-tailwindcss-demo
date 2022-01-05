622af7d6378fdd83f73b6aaced04764acf3ad1c1
Cuong Phan <codyreactista@gmail.com>
2022-01-06 00:17:06 +0700

    added basic seo

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Hero from "../components/Hero";
 import ProductList from "../components/ProductList";
 import { getProductsInCollection } from "../lib/shopify";
@@ -5,6 +6,34 @@ import { getProductsInCollection } from "../lib/shopify";
 export default function Home({ products }) {
   return (
     <div>
+      <Head>
+        <title>Modern eCommerce Course</title>
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta
+          httpEquiv="Content-Type"
+          content="text/html; charset=ISO-8859-1"
+        />
+        <meta
+          name="description"
+          content="Modern eCommerce Development Course focusing on Shopify, Next.js, TailwindCSS, GraphQL. Additional topics include Storefront API, Static Site Generation, getStaticPaths, getStaticProps and more."
+        />
+        <meta property="og:title" content="Modern eCommerce Course" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://shopify-nextjs-tailwindcss-demo.vercel.app"
+        />
+        <meta
+          property="og:image"
+          content="https://shopify-nextjs-tailwindcss-demo.vercel.app/share.png"
+        />
+        <meta
+          property="og:description"
+          content="Modern eCommerce Development Course focusing on Shopify, Next.js, TailwindCSS, GraphQL. Additional topics include Storefront API, Static Site Generation, getStaticPaths, getStaticProps and more."
+        />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="Modern eCommerce Course" />
+      </Head>
       <Hero />
       <ProductList products={products} />
     </div>
