6f1dab86112a57a35f2b5193554f9fa50674b670
Cuong Phan <codyreactista@gmail.com>
2022-01-18 23:41:01 +0700

    changed price formatter to vnd

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,4 @@
-export const formatter = new Intl.NumberFormat("en-US", {
+export const formatter = new Intl.NumberFormat("vi-VN", {
   style: "currency",
-  currency: "USD",
-  minimumFractionDigits: 2,
+  currency: "VND",
 });
